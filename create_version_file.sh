4324edacf85ea7f53b08ab23889252427173da12
just_lofe <97516390+justlofe@users.noreply.github.com>
2024-05-18 21:05:54 +0700

    Update create_version_file.sh

--- create_version_file.sh
+++ create_version_file.sh
@@ -3,11 +3,5 @@
 # Получаем номер последнего коммита
 commit_hash=$(git rev-parse HEAD)
 
-# Путь к папке ресурсов
-resources_dir="src/main/resources"
-
-# Создаем директорию, если она еще не существует
-mkdir -p "$resources_dir"
-
-# Создаем файл version.txt в директории ресурсов и записываем в него номер коммита
-echo "$commit_hash" > "$resources_dir/version.txt"
+# Создаем файл version.txt и записываем в него номер коммита
+echo "$commit_hash" > version.txt
