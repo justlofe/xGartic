14afde3339fc864d33d287bd5ee18fba2effa830
just_lofe <97516390+justlofe@users.noreply.github.com>
2024-05-19 22:39:49 +0700

    Delete create_version_file.sh

--- create_version_file.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Получаем номер последнего коммита
-commit_hash=$(git rev-parse HEAD)
-
-# Создаем файл version.txt и записываем в него номер коммита
-echo "$commit_hash" > version.txt
